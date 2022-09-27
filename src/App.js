c3c5e1c523f2463935a87d7c149ee89ff927e874
zainahda <zainahda@gmail.com>
2022-09-27 17:44:52 +0700

    Layouting apps

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import contactsJSON from './data/contacts.json';
+import { Grid, Container } from "@mui/material";
+import Header from "./components/Header";
+// import contactsJSON from './data/contacts.json';
+import ContactForm from "./components/ContactForm";
+import Contact from "./components/Contact";
 
 // Uncomment untuk memuat daftar kontak
 // import contactsJSON from './data/contacts.json';
@@ -8,11 +15,31 @@ const App = () => {
   // https://mui.com/material-ui/react-list/#folder-list
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
+  const [contacts, setContacts] = useState(contactsJSON);
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+  const addContact = (dContacts) => {
+    const newContacts = [...contacts, dContacts];
+    setContacts(newContacts);
+  }
 
   return (
+    <>
     <div className="App">
+      <Header />
+      <Container>
+        <Grid container spacing={2} columns={16}>
+          <Grid item xs={8}>
+            <ContactForm addContact={addContact} />
+          </Grid>
+          <Grid item xs={8}>
+            {contacts.map((contact, item) =>
+              <Contact key={item} data={contact} /> 
+            )}
+          </Grid>
+        </Grid>
+        </Container>
     </div>
+    </>
   );
 };
 

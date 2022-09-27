25c569658bff06f79d6eada6207872262c9d0c2b
zainahda <zainahda@gmail.com>
2022-09-27 17:35:43 +0700

    Edit header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
-import React from 'react';
+import { Box, Typography} from "@mui/material";
+import React from "react";
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <Box
+      sx={{
+        width: "auto",
+        height: "auto",
+      }}
+      my={5}
+    >
+      <Typography variant="h1" >Call a Friend</Typography>
+      <Typography variant="h5" mt={5}>Your friendly contact app</Typography>
+    </Box>
   );
 };
 
